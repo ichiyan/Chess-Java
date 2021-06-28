0ed81e25dd75ba456c538fbb1f9d03094941a82b
evad-e <cd.empinado@gmail.com>
2021-06-29 00:36:55 +0800

    king update

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -24,14 +24,18 @@ public class King extends Piece {
     @Override
     public boolean canMove(int destination_x, int destination_y)
     {
-        Piece testPiece = board.getPiece(destination_x, destination_y);
+        Piece testPiece;
         int i;
     // Remember: a king can move one square up, right, left, or down, or
     // diagonally, but he can never put himself in danger of an oposing 
     // piece attacking him on the next turn. He cannot attack his own pieces.
         
     //check if a same-color piece is on the destination
-    if(Math.abs( destination_x-this.getX()) == 1|| Math.abs( destination_y-this.getY())  == 1 ){;
+
+    if(Math.abs( destination_x-this.getX()) > 1 || Math.abs( destination_y-this.getY())  > 1 ){
+        return false;
+    }else{
+        testPiece = board.getPiece(destination_x, destination_y);
         if(testPiece!=null){
             if(testPiece.isWhite()&&this.isWhite()){
                 return false;
@@ -39,90 +43,28 @@ public class King extends Piece {
                 return false;
             }
         }
-
-       
-        
-        
-    }else if(destination_y==this.getY()&&destination_x-this.getX()==Math.abs(2)){
-        if(destination_x-this.getX()>0){
-           
-        }      
-    }else{
-        return false;
     }
-   
-
-    //moving north white perspective()
-    if(destination_y > this.getY()){
-        testPiece = board.getPiece(this.getX(), this.getY()+1);
-        if(testPiece!=null){
-            return false;
-        }
-    }
-    //moving east white perspective()
-    else if(destination_x>this.getX()){
-        testPiece = board.getPiece(this.getX()+1, this.getY());
-        if(testPiece!=null){
-            return false;
-        }
-    }
-     //moving south white perspective()
-    else if(destination_x<this.getY()){
-        testPiece = board.getPiece(this.getX(), this.getY()-1);
-        if(testPiece!=null){
-            return false;
-        }
-    }
-     //moving west white perspective()
-    else if(destination_x<this.getX()){
-        testPiece = board.getPiece(this.getX()-1, this.getY());
-        if(testPiece!=null){
-            return false;
-        }
-    }
-    //moving northeast white perspective()
-    else if(destination_y>this.getY() && destination_x>this.getX()){
-        testPiece = board.getPiece(this.getX()+1, this.getY()+1);
-        if(testPiece!=null){
-            return false;
-        }
-    }
-    //moving southeast white perspective()
-    else if(destination_y<this.getY() && destination_x>this.getX()){
-        testPiece = board.getPiece(this.getX()+1, this.getY()-1);
-        if(testPiece!=null){
-            return false;
-        }
-    }
-    //moving southwest white perspective()
-    else if(destination_y<this.getY() && destination_x<this.getX()){
-        testPiece = board.getPiece(this.getX()-1, this.getY()-1);
-        if(testPiece!=null){
-            return false;
-        }
-    }
-    //moving northwest white perspective()
-    else if(destination_y<this.getY() && destination_x<this.getX()){
-        testPiece = board.getPiece(this.getX()-1, this.getY()+1);
-        if(testPiece!=null){
-            return false;
-        }
-    }
-           
-        return true;
+  
+    return true;
     }
     public boolean canCastle(int destination_x, int destination_y){
-        int i;
         
+        int i;
+        Spot move;
         
         if(destination_x-this.getX()>0){
-           for(i=this.getX();i<destination_x;i++){
+           for( i=this.getX();i<destination_x;i++){
               if(this.isWhite()){
-                  
+                  move = new Spot(i,destination_y);
+                  for(Piece piece: board.Black_Pieces){
+                    if(piece.availableMoves().contains(move)){
+                        return false;
+                    }
+                  }
               }
            }
         } 
         return true;
     }
-
+    
 }

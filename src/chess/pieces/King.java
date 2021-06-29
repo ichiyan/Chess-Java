80328ae05de7d83a9bedea60e566f94aaaa537f9
evad-e <cd.empinado@gmail.com>
2021-06-29 12:58:16 +0800

    king update

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -26,14 +26,18 @@ public class King extends Piece {
     {
         Piece testPiece;
         int i;
-    // Remember: a king can move one square up, right, left, or down, or
-    // diagonally, but he can never put himself in danger of an oposing 
-    // piece attacking him on the next turn. He cannot attack his own pieces.
-        
-    //check if a same-color piece is on the destination
+    
 
     if(Math.abs( destination_x-this.getX()) > 1 || Math.abs( destination_y-this.getY())  > 1 ){
-        return false;
+        
+        if(Math.abs(destination_x-this.getX())==2 && destination_y==this.getY()){
+            if(!this.has_moved){
+                return canCastle();
+            }
+        }else{
+            return false;
+        }
+        
     }else{
         testPiece = board.getPiece(destination_x, destination_y);
         if(testPiece!=null){
@@ -47,24 +51,33 @@ public class King extends Piece {
   
     return true;
     }
-    public boolean canCastle(int destination_x, int destination_y){
+    public boolean canCastle(){
         
         int i;
-        Spot move;
+        for(i=this.getX();i<=this.getX()+2 && willHit(i, this.getY());i++){
+            if(board.getPiece(i, this.getY())!=null){
+                return false;
+            }
+        }
+       //can castle queen side or king side --not yet working
         
-        if(destination_x-this.getX()>0){
-           for( i=this.getX();i<destination_x;i++){
-              if(this.isWhite()){
-                  move = new Spot(i,destination_y);
-                  for(Piece piece: board.Black_Pieces){
-                    if(piece.availableMoves().contains(move)){
-                        return false;
-                    }
-                  }
-              }
-           }
-        } 
+              
+                     
         return true;
     }
-    
+    public boolean willHit(int x, int y){
+        Spot move;
+        if(this.isWhite()){
+            move = new Spot(x,y);
+            for(Piece piece: board.Black_Pieces){
+                for(Spot spot: piece.availableMoves()){
+                    if(move.getX() == spot.getX() && move.getY() == spot.getY()){
+                        System.out.println("true");
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 }
